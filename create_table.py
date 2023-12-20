6b5d10d23651a1fa61abe234973c15540bd46f8f
AleksandrZelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2023-12-20 17:59:25 +0000

    Update apps

--- create_table.py
+++ create_table.py
@@ -14,7 +14,7 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print("Connected to database successfully")
 
-conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, zip TEXT)')
+conn.execute('CREATE TABLE if not exists students (name TEXT, addr TEXT, city TEXT, zip TEXT)')
 print("Created table successfully!")
 
 conn.close()
