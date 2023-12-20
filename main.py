76098072e311732c24aa4d663d48273a9b3e1914
Aleksandr <alexandr44@inbox.lv>
2023-12-20 19:27:50 +0200

    update apps

--- main.py
+++ main.py
@@ -136,3 +136,7 @@ def delete():
             con.close()
             # Send the transaction message to result.html
             return render_template('result.html',msg=msg)
+        
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
